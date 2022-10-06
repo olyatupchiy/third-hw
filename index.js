a5464082291e05a983520c02e01e61837288083d
olyatupchiy <olya.tupchiy@gmail.com>
2022-10-06 13:05:44 +0200

    Added sum of numbers in js file

---
+++ index.js
@@ -0,0 +1,4 @@
+const firstNumber = parseInt(prompt('Enter the first number'));
+const secondNumber = parseInt(prompt('Enter the second number'));
+const result = firstNumber + secondNumber;
+alert(result);
